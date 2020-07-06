57ae0bd0c24a75764aa421af3150bc72d68e1b27
Mohib-Arsala <mohib-arsala@Mohib-Arsalas-MacBook-Pro.local>
2020-07-05 23:28:24 -0400

    React Project added

---
+++ build/precache-manifest.e93b53bb8ed47ae9b5e8467acab2ecac.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "756acf7943d38e6accac34e2be6ee670",
+    "url": "/index.html"
+  },
+  {
+    "revision": "09bfc73bc9bbefd3635b",
+    "url": "/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "6502b1eff9d98777f933",
+    "url": "/static/css/main.7de25668.chunk.css"
+  },
+  {
+    "revision": "09bfc73bc9bbefd3635b",
+    "url": "/static/js/2.c2bb7636.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.c2bb7636.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6502b1eff9d98777f933",
+    "url": "/static/js/main.6ba38a84.chunk.js"
+  },
+  {
+    "revision": "8908b456e0959290f9d5",
+    "url": "/static/js/runtime-main.65611daa.js"
+  }
+]);
---
+++ src/Card.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+
+const Card = () => {
+    return (
+        <div className="f1 tc">
+            <img alt="cars" src="https://robohash.org/test?200x200" /> 
+            <div>
+                <h2>Toyota</h2>
+                <p>Specification</p>
+            </div>
+        </div>
+    );
+}
+
+export default Card;
