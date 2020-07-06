1bc6969e4a03a17cd59868667cdeeb0c9887e61c
Mohib-Arsala <mohib-arsala@Mohib-Arsalas-MacBook-Pro.local>
2020-07-06 00:31:43 -0400

    car file added

--- src/Card.js
+++ src/Card.js
@@ -1,13 +1,13 @@
 import React from "react";
 
 
-const Card = () => {
+const Card = (props) => {
     return (
-        <div className="f1 tc">
+        <div className="bg-light-blue dib br3 pa3 grow f1 tc bw2 shadow-5">
             <img alt="cars" src="https://robohash.org/test?200x200" /> 
             <div>
-                <h2>Toyota</h2>
-                <p>Specification</p>
+                <h2>{props.name}</h2>
+                <p>{props.email}</p>
             </div>
         </div>
     );
