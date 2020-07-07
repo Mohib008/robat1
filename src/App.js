4a6164a5024247363fd5c282faf9efa3f99f8465
Mohib Arsala <mohib.0008@gmail.com>
2020-07-07 19:18:24 -0400

    App file updated

--- src/App.js
+++ src/App.js
@@ -1,17 +1,31 @@
-import React from "react";
+import React, { Component } from "react";
 import CardList from "./CardList";
 import SearchBox from "./SearchBox";
 import { car } from "./car";
 
 
-const App = () => {
-    return (
-        <div className="tc">
-            <h1>Cars & Robots!</h1>
-            <SearchBox />
-            <CardList car = {car} />
-        </div>
-    );
+class App extends Component {
+    constructor(){
+        super()
+        this.state = {
+            car: car,
+            searchfield: ""
+        }
+    }
+
+    onSearchChange(event){
+        console.log(event.target.value);
+    }
+
+    render() {
+        return (
+            <div className="tc">
+                <h1>Cars & Robots!</h1>
+                <SearchBox searchChange={this.onSearchChange}/>
+                <CardList car = {this.state.car} />
+            </div>
+        );
+    }
 }
 
 export default App;
