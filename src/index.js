10f100e02b892fc4a0edbf2ced801e0b3a2295d0
Mohibullah Arsala <mohib-arsala@Mohibullahs-MacBook-Pro.local>
2020-07-17 00:36:46 -0400

    middaleware installed

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from "react-redux";
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
+import { createLogger } from "redux-logger";
 import './index.css';
 import App from "./containers/App";
 import * as serviceWorker from './serviceWorker';
 import { searchCar } from "./reducers";
 import "tachyons";
 
-const store = createStore(searchCar);
+const logger = createLogger();
+const store = createStore(searchCar, applyMiddleware(logger))
 
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 
